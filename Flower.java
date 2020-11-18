dc7c2dd8987d3b5dd17183384eac90ca92a08c79
muralishankar1999 <74663652+muralishankar1999@users.noreply.github.com>
2020-11-18 19:19:24 +0530

    Add files via upload
    examples `for classes and object

---
+++ Flower.java
@@ -0,0 +1,9 @@
+class Flower{
+  String name;
+  String colour;
+
+  void blooming(){
+System.out.println(name+"is bloooming and it is "+colour+" in colour");
+}
+
+}
---
+++ Flowerdemo.java
@@ -0,0 +1,13 @@
+
+class Flowerdemo{
+ public static void main(String args[]){
+Flower f1 = new Flower();
+f1.name="Rose";
+f1.colour="Red";
+f1.blooming();
+Flower f2 = new Flower();
+f2.name="sun flower";
+f2.colour="saffron";
+f2.blooming();
+}
+}
